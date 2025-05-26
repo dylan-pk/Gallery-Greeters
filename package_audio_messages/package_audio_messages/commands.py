44e40781dadf27c7ca7ed2ad457ed5278f138bdd
Anika Roth <anika.roth@student.uts.edu.au>
2025-05-26 17:30:27 +1000

    enitre audio_messages stuff turned into a package, should fix relative pathing issues

---
+++ package_audio_messages/package_audio_messages/commands.py
@@ -0,0 +1,372 @@
+import speech_recognition as sr
+import pyttsx3
+import pyaudio
+import random
+import time
+from PIL import Image, ImageTk, ImageDraw, ImageFont
+import tkinter as tk
+from .tableOrganisation import TableDatabase
+from screeninfo import get_monitors
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import PoseStamped
+from nav_msgs.msg import Path
+from builtin_interfaces.msg import Time
+from std_msgs.msg import Int32
+import math
+from geometry_msgs.msg import PoseStamped, Quaternion
+from ament_index_python.packages import get_package_share_directory
+import os
+import ast
+
+
+import threading
+
+DURATION = 2000
+WPM = 160
+GREETLOCATION = [0.66,0.82, -1.57]
+CHARGINGLOCATION = [3.1, 3.25]
+SENTRYMODE = [[3.34, 2.45],[3.36, 1.76],[3.38, 1.75],[3.0, 0.88],[1.78, 0.93],[0.6, 1.05],[0.48, 2.61],[2.0, 2.39], [3.34, 2.45]]
+
+
+
+class Commands:
+
+    def __init__(self, node, gui_queue):
+        self.node = node # saved for reference
+        self.queue = gui_queue
+
+        self.device = pyaudio.PyAudio()
+        # initialising a recogniser
+        self.r = sr.Recognizer()
+        self.engine = pyttsx3.init()
+        # self.testingVoices()
+        self.engine.setProperty('rate',WPM)
+        voices = self.engine.getProperty('voices')
+        self.engine.setProperty('voice', voices[19].id)
+
+        ## ROS Topics and stuff
+        self.subscriber_currentPos = node.create_subscription(Odometry, '/odom', self.odom_callback, 10)
+        self.currentPos = [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0]]
+        self.waiter_pub = node.create_publisher(PoseStamped, '/waiter_tableloc', 10)
+        # self.drinks_pub = node.creat_publisher(DrinksOrder, '/drink_orders', 10)
+
+        # publishers to talk to other subsystems
+        self.publisher_movementMode = node.mode_pub
+        self.publisher_location = node.goal_pub
+        self.publisher_path = node.publisher_path
+
+        # monitor = get_monitors()[1] # this will allow us to put it on the second screen
+        self.display_event = threading.Event()
+        monitors = get_monitors()
+        self.root = tk.Tk()
+        # print(moSnitors)
+        if len(monitors) > 1:
+            maxx = 0
+            for i, mon in enumerate(monitors):
+                if mon.x != 0 and mon.x > maxx:
+                    maxx = mon.x
+                    self.monitor = monitors[i]
+            
+            self.screenWidth, self.screenHeight = self.monitor.width, self.monitor.height
+            self.root.geometry(f"{self.monitor.width}x{self.monitor.height}+{self.monitor.x}+{self.monitor.y}") # attaching the root to the second screen
+            self.root.attributes('-fullscreen', True)
+            self.root.bind("<Escape>", lambda e: self.root.destroy())
+        else:
+            # Data needed to make images halfscreen
+            self.screen_width = self.root.winfo_screenwidth()
+            self.screen_height = self.root.winfo_screenheight()
+            # Half size
+            self.half_width = self.screen_width // 2
+            self.screenWidth= self.half_width
+            self.half_height = self.screen_height // 2
+            self.screenHeight= self.half_height
+            self.root.geometry(f"{self.half_width}x{self.half_height}")
+        
+        # Establishing the root used
+        self.root.after(100, self.process_gui_queue)
+        
+        ## Establishing all the images
+        self.loadInfo()
+
+        self.tk_default_image = ImageTk.PhotoImage(self.default_image)
+        self.label = tk.Label(self.root, image=self.tk_default_image)
+        self.label.pack(expand=True)    
+        self.fullScreenImage(self.default_image,10) 
+        # self.runGUI()   
+
+################################################################### Base Functionality Code #########################################################################
+    def process_gui_queue(self):
+        while not self.queue.empty():
+            try:
+                func = self.queue.get()
+                func()
+            except Exception as e:
+                print(f"GUI Error: {e}")
+        self.root.after(100, self.process_gui_queue)
+
+    def pushToQueue(self, file, duration, manualReset=False):
+        self.display_event.clear()
+        # print(f"display event cleared so it's {self.display_event.is_set()}")
+        self.queue.empty()
+        self.queue.put(lambda: self.fullScreenImage(file, duration, manualReset))
+
+    def getNumofTables(self):
+        return self.tables.getNumofTables()
+    
+    def speakingTimeEst(self, text):
+        words = len(text.split())
+        wps = WPM / 60
+        return int(((words / wps)+0.25) * 1000)
+        
+    def loadInfo(self):
+        package_path = get_package_share_directory('package_audio_messages')
+        resources_path = os.path.join(package_path, 'resources')
+
+        self.default_image = Image.open(os.path.join(resources_path, 'default.png'))
+        self.dancingFace = Image.open(os.path.join(resources_path, 'danceFace.png'))
+        self.chargeFace = Image.open(os.path.join(resources_path, 'chargeFace.png'))
+        self.callingFace = Image.open(os.path.join(resources_path, 'callingWaiter.png'))
+
+        # Load artwork info
+        artwork_info_path = os.path.join(resources_path, 'ArtworkInfo', 'artworkInfo.txt')
+        with open(artwork_info_path, 'r') as artFile:
+            self.artNames = artFile.readline().split("/")
+            self.artLocation = artFile.readline().split("/")
+            self.artworks = {
+                name: ast.literal_eval(loc)
+                for name, loc in zip(self.artNames, self.artLocation)
+            }
+            self.artInfo = artFile.readlines()
+
+        self.artImages = {
+            name: Image.open(os.path.join(resources_path, 'ArtworkInfo', f"{name.strip()}_info.png"))
+            for name in list(self.artworks.keys())[:5]
+        }
+
+        # Load fun facts
+        funfacts_path = os.path.join(resources_path, 'FunFacts', 'funfacts.txt')
+        with open(funfacts_path, 'r') as factFile:
+            self.facts = factFile.readlines()
+
+        self.factImages = {
+            i: Image.open(os.path.join(resources_path, 'FunFacts', f"image{i}.png"))
+            for i in range(10)
+        }
+
+        # Load table info
+        table_info_path = os.path.join(resources_path, 'tableInfo.txt')
+        self.tables = TableDatabase(table_info_path)
+        # Robot Faces
+        # self.default_image = Image.open("audio_messages/src/resources/default.png")#.resize((self.half_width, self.half_height), Image.ANTIALIAS)
+        # self.dancingFace = Image.open("audio_messages/src/resources/danceFace.png")#.resize((self.half_width, self.half_height), Image.ANTIALIAS)
+        # self.chargeFace = Image.open("audio_messages/src/resources/chargeFace.png")#.resize((self.half_width, self.half_height), Image.ANTIALIAS)
+        # self.callingFace = Image.open("audio_messages/src/resources/callingWaiter.png")#.resize((self.half_width, self.half_height), Image.ANTIALIAS)
+
+        # # Information about Artwork        
+        # with open("audio_messages/src/resources/ArtworkInfo/artworkInfo.txt", "r") as artFile:
+        #     self.artNames = artFile.readline().split("/")
+        #     self.artLocation = artFile.readline().split("/")
+        #     self.artworks = {}
+        #     for i, l in zip(self.artNames, self.artLocation):
+        #         loc = ast.literal_eval(l)
+        #         self.artworks[i] = loc
+        #         # self.artworks.update({i: l})
+        #     print(self.artworks)
+        #     self.artInfo = artFile.readlines()
+        #     # print(self.artInfo)
+        # # self.artImages = {a: Image.open(f"audio_messages/src/resources/ArtworkInfo/{self.artworks[a].strip()}_info.png")#.resize((self.half_width, self.half_height), Image.ANTIALIAS)
+        # #                    for a in range(5)}
+        # self.artImages = {name: Image.open(f"audio_messages/src/resources/ArtworkInfo/{name.strip()}_info.png") for name in list(self.artworks.keys())[:5]}
+        # # print(self.artImages)
+
+        # # Fun Facts
+        # with open("audio_messages/src/resources/FunFacts/funfacts.txt", "r") as factFile:
+        #     self.facts = factFile.readlines()
+        # self.factImages = {i: Image.open(f"audio_messages/src/resources/FunFacts/image{i}.png")#.resize((self.half_width, self.half_height), Image.ANTIALIAS)
+        #                    for i in range(10)}
+        
+        # # Table Database
+        # self.tables = TableDatabase("audio_messages/src/resources/tableInfo.txt")
+    
+    def getArtworkNames(self):
+        return self.artworks
+    
+    def establishLocations(self, location_x, location_y, theta = None):
+        goal = PoseStamped()
+        goal.header.frame_id = "map"
+        goal.header.stamp = self.node.get_clock().now().to_msg() 
+        goal.pose.position.x = location_x
+        goal.pose.position.y = location_y
+        if theta is not None:
+            qz = math.sin(theta / 2.0)
+            qw = math.cos(theta / 2.0)
+
+            goal.pose.orientation = Quaternion(x=0.0, y=0.0, z=qz, w=qw)
+        else:
+            goal.pose.orientation.w = 1.0
+
+        return goal
+
+    def runGUI(self):
+        try:
+            while True:
+                self.root.update_idletasks()
+                self.root.update()
+                time.sleep(0.01)
+        except tk.TclError:
+            print("GUI closed.")
+
+    def fullScreenImage(self, newImage, duration=4000, manualReset= False):
+        # print(duration)
+        def _update_image():
+            try:
+                # Create the PhotoImage on the main thread
+                tk_image = ImageTk.PhotoImage(newImage)
+
+                # Store a reference to prevent garbage collection
+                self.label.image = tk_image
+                self.label.configure(image=tk_image)
+                # print(f"display event before {self.display_event.is_set()}")
+                self.display_event.set()
+                # print(f"display event after {self.display_event.is_set()}")
+                # Optionally reset image after duration
+                if not manualReset:
+                    self.root.after(duration, self.resetToDefault)
+            except Exception as e:
+                print(f"[fullScreenImage] Error updating image: {e}")
+
+        # Always call GUI logic via the main thread
+        self.queue.put(_update_image)
+
+    def resetToDefault(self):
+        # print("reseting to default")
+        self.label.configure(image=self.tk_default_image)
+        self.label.image = self.tk_default_image
+
+    # Convert Text to Speech
+    def SpeakText(self, command, wait_for_image=False):
+        if wait_for_image and self.display_event is not None:
+            self.display_event.wait()
+        else:
+            time.sleep(0.5)
+        # print("speaking now")
+        self.engine.say(command)
+        # The say function does not work without a run and wait command
+        self.engine.runAndWait()
+        # self.display_event.clear()
+        time.sleep(0.25)
+
+#################################################################### ROS CALLBACKS ################################################################################
+    def odom_callback(self, msg):
+        position = msg.pose.pose.position
+        orientation = msg.pose.pose.orientation
+        # print(f"Position: x = {position.x}, y = {position.y}, z = {position.z}")
+        # print(f"Orientation: x = {orientation.x}, y = {orientation.y}, z = {orientation.z}, w = {orientation.w}")
+        self.currentPos = [[position.x, position.y, position.z], [orientation.x, orientation.y, orientation.z, orientation.w]]
+############################################################## SPECIFIC COMMAND FUNCTONS ##########################################################################
+    
+    def modeChange(self, mode):
+        ros_msg = Int32()
+        ros_msg.data = mode
+        match mode:
+            case 0: # Go to Artwork
+                self.publisher_movementMode.publish(ros_msg)
+                print("Entered Artwork Mode")
+
+            case 1: # Wander Around
+                self.publisher_movementMode.publish(ros_msg)
+                print("Wander Command Recognised")
+                self.SpeakText("beginning sentry mode")
+                self.sentryMode()
+
+            case 2: # Dance Mode
+                self.publisher_movementMode.publish(ros_msg)
+                print("dance mode activated")
+                self.pushToQueue(self.dancingFace,DURATION)
+                self.SpeakText("Dancey Dancey")
+
+            case 3: # Spin around for Artwork
+                self.publisher_movementMode.publish(ros_msg)
+                print("Spinning activated")
+
+    def sentryMode(self):
+        path_msg = Path()
+        path_msg.header.frame_id = 'map'
+        path_msg.header.stamp = self.node.get_clock().now().to_msg() 
+
+        for goal in SENTRYMODE:
+            goal_pose = PoseStamped()
+            goal_pose = self.establishLocations(goal[0], goal[1])
+            path_msg.poses.append(goal_pose)
+        
+        self.publisher_path.publish(path_msg)
+
+    def sendDrinkOrder(self, drinks, table):
+        for i in range(3):
+            print("Ordering " + str(drinks[i][1]) + " " + str(drinks[i][0]) + "s")
+        # self.drinks_pub.publish(drinks)
+
+    def publishArtLocation(self, artwork):
+        print(f"publishing location: [{self.artworks[artwork][0]}, {self.artworks[artwork][1]}, {self.artworks[artwork][2]}]")
+        self.publisher_location.publish(self.establishLocations(float(self.artworks[artwork][0]), float(self.artworks[artwork][1]), float(self.artworks[artwork][2])))
+    
+    def artWorkInfo(self, artwork):
+        # Read closest artwork from publisher
+        print(f"Artwork Info Command Registered, artwork recieved is {artwork}")
+        idx = 0
+        idx = int(idx)
+        for name, location in self.artworks.items():
+            print(f"index: {idx}, name: {name}, location x: {location[0]}")
+            if name == artwork:
+                # self.publisher_location.publish(self.establishLocations(float(location[0]), float(location[1]), float(location[2])))
+                self.pushToQueue(self.artImages[name],(self.speakingTimeEst(self.artInfo[idx]) + DURATION))
+                self.SpeakText(self.artInfo[idx].lower)
+                break
+            idx += 1
+
+    def tableStatus(self):
+        print("Table Status Command Registered")
+        statusImage = self.tables.generateTableStatusImage(self.screenWidth, self.screenHeight)
+        self.pushToQueue(statusImage, DURATION)
+
+    def callWaiter(self):
+        roboLocation = self.currentPos[1]
+        print(roboLocation)
+        table = self.tables.getClosestTable(roboLocation)
+        # table = 2
+        print(f"Call Waiter Command Recognised, calling to table {table}")
+        self.pushToQueue(self.callingFace, DURATION)
+        self.SpeakText(f"calling a waiter to table {table}")
+        tableLocation = self.tables.findTableLocation(table)
+        self.waiter_pub.publish(self.establishLocations(tableLocation[0], tableLocation[1]))
+
+    def goToTable(self, table):
+        tableLocation = self.tables.findTableLocation(table)
+        self.SpeakText(f"follow me to table {str(table)}")
+        print(f"Go To Table Command Recognised, Table Location is: {tableLocation}")
+        self.publisher_location.publish(self.establishLocations(tableLocation[0], tableLocation[1]))
+
+
+    def funFact(self):
+        factNum = random.randint(0,9)
+        # Outputting the fact image and audio
+        print("Fun Fact " + self.facts[factNum])
+        self.pushToQueue(self.factImages[factNum], (self.speakingTimeEst(self.facts[factNum]) + DURATION))
+        # self.fullScreenImage(self.factImages[factNum],(self.speakingTimeEst(self.facts[factNum]) + DURATION))
+
+        self.SpeakText(self.facts[factNum], wait_for_image=True)
+    
+    def triggerGreet(self):
+        print("Greeting Guests Command Recognised")
+        self.SpeakText("going to door to greet guests")
+        self.publisher_location.publish(self.establishLocations(GREETLOCATION[0], GREETLOCATION[1]))
+        
+    def triggerCharge(self):
+        print("Charging Command Recognised")
+        self.pushToQueue(self.chargeFace,DURATION)
+        self.SpeakText("returning to charging port")
+        self.publisher_location.publish(self.establishLocations(CHARGINGLOCATION[0], CHARGINGLOCATION[1]))
+        
+
+
+# comms = Commands()
